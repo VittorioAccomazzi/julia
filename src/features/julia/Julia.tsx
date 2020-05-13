4a327f6a9ac5e0c890f2096e9e83c30081dff1af
Vittorio Accomazzi <Vittorio.Accomazzi@gmail.com>
2020-05-12 20:04:00 -0400

    re #2 -- second attempt for animation

--- src/features/julia/Julia.tsx
+++ src/features/julia/Julia.tsx
@@ -16,27 +16,32 @@ import {
 
     type JuliaProps = { lut : Lut } & WithStyles<typeof styles>;
 
-    const resetTime  = 8000; // a new animation every 8 seconds.
+    const resetTime  = 3000; // a new animation every 5 seconds.
     const frameTime = 200;  // 5 frame sec.
     const maxDistance= 10; // in sampling step (pixels)
+    const noValue : number = -1;
 
     const Julia = ({lut, classes } : JuliaProps) => {
-        let cStart = useRef<C>({x:0,y:0});
-        let cEnd   = useRef<C>({x:0,y:0});
+        let cStart = useRef<C>({x:-2,y:0});
+        let cEnd   = useRef<C>({x:-2,y:0});
         let pos    = useRef<number>(0);
-        let [cPoint, setCPoint] = useState<C>({x:0, y:0})
+        let iEnd   = useRef<number>(noValue);
+        let [cPoint, setCPoint] = useState<C|null>(null)
 
         // function to init the animation
         const resetPoints= () =>{
             let sIndex = Math.floor(cPoints.seeds.length * Math.random());
+            if( iEnd.current != noValue ) sIndex = iEnd.current;
             let sPoint = cPoints.seeds[sIndex];  
             let dst = maxDistance * (cPoints.xMax-cPoints.xMin)/cPoints.nx;
-            let neigbours = cPoints.seeds.filter( (pt,i) => i != sIndex && Math.abs(pt.x - sPoint.x) < dst && Math.abs( pt.y - sPoint.y ) < dst );
-            let ePoint = {x:sPoint.x + Math.random() * dst/2, y: sPoint.y*Math.random()*dst/2, i:0} // just to set an end point.
-            if( neigbours.length > 0 ) ePoint = neigbours[Math.floor(neigbours.length*Math.random())];
-            let ySign = Math.random() < 0.5 ? 1 : -1;
-            sPoint.y *= ySign;
-            ePoint.y *= ySign;
+            let nMap = cPoints.seeds.map( (pt,i) => (i != sIndex && Math.abs(pt.x - sPoint.x) < dst && Math.abs( pt.y - sPoint.y ) < dst) ? i : noValue );
+            let nIndexes = nMap.filter((i)=> i!=noValue);
+            let ePoint = {x:sPoint.x + Math.random() * dst/2, y: sPoint.y+Math.random() *dst/2, i:0} // just to set an end point.
+            iEnd.current = noValue;
+            if( nIndexes.length > 0 ){
+                iEnd.current = nIndexes[Math.floor(nIndexes.length*Math.random())];
+                ePoint = cPoints.seeds[iEnd.current];
+            } 
             cStart.current = sPoint;
             cEnd.current = ePoint;
             pos.current = 0;
@@ -77,7 +82,9 @@ import {
         },[]);
 
         return (
-                <JFractal c={cPoint} lut={lut} />
+            <>
+                { cPoint != null && (<JFractal c={cPoint} lut={lut} />) }
+            </>
         );
     }
 
